6ef93edf9b57d5d0938d1d60535a910a3802d328
01ritvik <60423384+01ritvik@users.noreply.github.com>
2021-09-14 20:25:20 +0530

    Add files via upload

---
+++ final.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+my_db = mysql.connector.connect(host = '119.18.55.154',user = 'shashik' , passwd = 'shashikpass' , database = 'shashi_db')
+
+print(my_db)
+
+cursor = my_db.cursor()
+cursor.execute('select NUMBER,message,date_format(TIME,"%H"),date_format(TIME,"%i") from whatsapp')
+data = cursor.fetchall()
+for x in data:
+    print(x)
+
+for x in data:
+    import pywhatkit as kt
+    import pyautogui as pg
+
+    kt.sendwhatmsg(x[0],x[1],int(x[2]),int(x[3]),wait_time=30)
+    pg.press('enter')
